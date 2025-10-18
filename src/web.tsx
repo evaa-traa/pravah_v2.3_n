897d4f7527ff2579296048565b3b70dfc24b10e4
[dyad] <www.rps9451@gmail.com>
2025-10-18 18:50:31 +0530

    [dyad] Fixed TypeScript compile-time errors - wrote 3 file(s), renamed 1 file(s)

--- src/web.tsx
+++ src/web.tsx
@@ -2,25 +2,25 @@
 import { parseChatbot, injectChatbotInWindow } from './window';
 import { ThemeProvider } from './context/ThemeContext';
 import { ChatLayout } from './components/layout/ChatLayout';
-import { customElement } from 'solid-element';
+import { customElement, ICustomElement } from 'solid-element'; // Re-import ICustomElement
 import { defaultBotProps } from './constants';
 import { Bubble, BubbleProps } from './features/bubble';
 import { Full, FullProps } from './features/full';
 import { JSX } from 'solid-js';
 
 // Define a wrapper for the Full component to include ThemeProvider and ChatLayout
-const FullWithLayout = (props: FullProps, options: { element: HTMLElement }): JSX.Element => {
+const FullWithLayout = (props: FullProps, options: { element: ICustomElement }): JSX.Element => { // Use ICustomElement
   return (
     <ThemeProvider>
       <ChatLayout>
-        <Full {...props} element={options.element} />
+        <Full {...props} element={options.element as HTMLElement} /> {/* Cast to HTMLElement */}
       </ChatLayout>
     </ThemeProvider>
   );
 };
 
 // Define a wrapper for the Bubble component to include ThemeProvider and ChatLayout
-const BubbleWithLayout = (props: BubbleProps, options: { element: HTMLElement }): JSX.Element => {
+const BubbleWithLayout = (props: BubbleProps, options: { element: ICustomElement }): JSX.Element => { // Use ICustomElement
   return (
     <ThemeProvider>
       <ChatLayout>
