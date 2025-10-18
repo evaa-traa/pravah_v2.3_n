a4c8216804183340156313e2f1e0a6e29aae6fa2
[dyad] <www.rps9451@gmail.com>
2025-10-18 20:13:24 +0530

    Updated src/components/Badge.tsx

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -48,7 +48,7 @@ export const Badge = (props: Props) => {
             'background-color': props.badgeBackgroundColor ?? '#ffffff',
           }}
         >
-          {props.footer?.text ?? 'Powered by'}
+          {props.footer?.text ?? 'Driven by'}
           <a
             ref={liteBadge}
             href={props.footer?.companyLink ?? 'mailto:mail.rps.active@proton.me'}
